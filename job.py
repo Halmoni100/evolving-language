5e8082f5230d31b1a4c775eacf40770f36f12cf4
eleanorye00 <junru_ye@brown.edu>
2023-05-06 15:09:26 -0400

    update job.py

--- job.py
+++ job.py
@@ -1,7 +1,7 @@
 from run_simple_spread import *
 
 if __name__ == '__main__':
-
+    main(500,2,200,False,False)
     main(500,2,200,True,False, copier_ep_lookback=2000)
     main(500,3,200,False,False)
     main(500,3,200,True,False, copier_ep_lookback=2000)
